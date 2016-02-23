473dedc80308fbdef5cde3ea716eb77ba6264f93
ilikealotgithub1234 <erikpmurdock@gmail.com>
2016-02-23 11:32:22 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+<script id = 'jQuery' src="http://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script>
+<script>
+      $(document).foundation();
+    </script>
+<script type="text/javascript" src="https://intercom.zurb.com/scripts/zcom.js"></script>
