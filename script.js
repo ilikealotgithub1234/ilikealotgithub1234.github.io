fc30c21efa19743254b9919e059639e97ef80ac4
ilikealotgithub1234 <erikpmurdock@gmail.com>
2017-09-23 14:17:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
-<script id = 'jQuery' src="http://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script>
-<script>
-      $(document).css('background','red');
-    </script>
-<script type="text/javascript" src="https://intercom.zurb.com/scripts/zcom.js"></script>
+
